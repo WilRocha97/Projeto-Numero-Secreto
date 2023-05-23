2323d334a959c9d67c9aedbf3b668ac273cf1dc0
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2023-05-23 11:12:40 -0300

    criando validações
    capturando o chute do usuário e validando os dados

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,32 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || 
+webkitSpeechRecognition;
+
+//configura a linguagem do reconhecimento de voz e incia a api
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+//captura o evento de fala
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    // captura o conteudo transcrito pela api
+    chute = e.result[0][0].transcript
+    //exibe na tela
+    exibeChuteNaTela(chute)
+    //valida o valor do chute
+    validaChute(chute)
+}
+
+//exibe na tela
+function elementoChute(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <div class="resultado">
+            <span class="box">${chute}</span>
+        </div>
+    `
+}
+
---
+++ app/validacao.js
@@ -0,0 +1,46 @@
+//valida o chute
+function validaChute(chute) {
+    const numero =+ chute
+
+    if (chuteInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido, deve ser um número.</div>'
+        return
+    }
+
+    if (maiorMenor(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido, fale um número entre ${menorValor} e ${maiorValor}.</div>`
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h1>Você acertou!</h1>
+        <span class="box">${numeroSecreto}</span>
+        `
+    }else if (numero < numeroSecreto) {
+        elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <div class="resultado">
+            <div>O número secreto é menor <i><</i></div>
+            <span class="box">${numero}</span>
+        </div>`
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <div class="resultado">
+            <div>O número secreto é maior <i>></i></div>
+            <span class="box">${numero}</span>
+        </div>`
+    }
+}
+
+//verifica se é um número
+function chuteInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+
+//verifica se está entre o menor e maior valor
+function maiorMenor(numero) {
+    return numero > maiorValor || numero < menorValor
+}
